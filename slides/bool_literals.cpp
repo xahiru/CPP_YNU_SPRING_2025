2bb0f75b79fac49752ce24df8613c5b1c7c9107b
Ahmed Zahir <xahiru@gmail.com>
2025-03-05 20:10:34 +0800

    bool literal

---
+++ slides/bool_literals.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	const bool isTrue = true;
+	const bool isFalse = false;
+
+	cout << "isTrue? "
+		<< isTrue << "\n";
+	cout << "isFalse? "
+		<< isFalse << "\n";
+
+	return 0;
+}
