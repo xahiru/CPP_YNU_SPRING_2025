a716871f6fb75ef7746f2cfa81768e421f9b4dc3
Ahmed Zahir <xahiru@gmail.com>
2025-03-05 20:10:03 +0800

    slide 45 added

---
+++ slides/slide45.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    int age = 39;
+    string name = "Yuannan univeristy";
+    char gender = 'M';
+    bool isMarried = true;
+    float weight = 68.5;
+    cout << name << "\n"
+        << age << "\n"
+        << gender << "\n"
+        << isMarried << "\n"
+        << weight << "\n";
+    return 0;
+}
