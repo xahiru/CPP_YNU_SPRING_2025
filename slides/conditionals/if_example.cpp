bb7a8ed7d49c01d2a3c620dbfaffdba0e0d7b00d
Ahmed Zahir <xahiru@gmail.com>
2025-03-12 09:35:13 +0800

    if example added

---
+++ slides/conditionals/if_example.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int age = 19;
+  
+  	// Check if age is greater than 18 fo
+  	// vote eligiblity
+    if (age > 18) {
+        cout << "allowed to vote";
+    }
+    return 0;
+}
