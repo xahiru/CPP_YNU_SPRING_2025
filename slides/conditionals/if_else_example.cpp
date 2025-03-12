139c7f4a67c53559b87d85a990f548ba19d4e923
Ahmed Zahir <xahiru@gmail.com>
2025-03-12 09:35:26 +0800

    if else example added

---
+++ slides/conditionals/if_else_example.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+
+    // Using if-else to determine if the number is positive
+    // or non positive
+    if (n > 0) {
+        cout << "number is positive.";
+    }
+    else {
+        cout << "number is non-positive.";
+    }
+    return 0;
+}
