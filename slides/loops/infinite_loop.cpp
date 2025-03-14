b040e9960b795e2df85b25ff126afbf520859387
Ahmed Zahir <xahiru@gmail.com>
2025-03-14 22:05:01 +0800

    infinite loop

---
+++ slides/loops/infinite_loop.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // This is an infinite for loop as the condition
+    // expression is blank
+    for (;;) {
+        cout << "This loop will run forever.\n";
+    }
+    return 0;
+}
