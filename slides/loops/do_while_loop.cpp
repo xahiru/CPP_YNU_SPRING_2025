22e51a18aaa7fdb80d46a7a5ffba3cbb2b24fc1a
Ahmed Zahir <xahiru@gmail.com>
2025-03-14 22:04:34 +0800

    do while loop

---
+++ slides/loops/do_while_loop.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+      // Initialization
+      int i = 1;
+  
+      // while loop that starts with i = 1 and ends
+      // when i is greater than 5.
+     do {
+        cout << i << " ";
+      
+          // Updation
+          i++;
+    }while (i <= 5);
+
+    return 0;
+}
