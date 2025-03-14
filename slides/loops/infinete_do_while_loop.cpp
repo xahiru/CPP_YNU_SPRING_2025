7c4de8c4bd5a607432788b5a9237ba340a5b058d
Ahmed Zahir <xahiru@gmail.com>
2025-03-14 22:06:28 +0800

    do while infinte loop

---
+++ slides/loops/infinete_do_while_loop.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // This is an infinite do while loop as the condition
+    // expression is always true
+    do {
+        cout << "This loop will run forever.\n";
+    }while(1);
+    return 0;
+}
