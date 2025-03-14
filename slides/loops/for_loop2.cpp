63d1c032accc7f634634afaebef5fbeaff3c161b
Ahmed Zahir <xahiru@gmail.com>
2025-03-14 22:02:38 +0800

    second for loop

---
+++ slides/loops/for_loop2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+      // For loop that starts with i = 1 and ends
+      // when i is greater than 5.
+    for (int i = 1; i <= 5; i++) {
+        cout << i << " ";
+    }
+
+    return 0;
+}
