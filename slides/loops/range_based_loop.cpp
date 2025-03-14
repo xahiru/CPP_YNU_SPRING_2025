b35ada7db0d9276f7046d6c58e3f2331d275000f
Ahmed Zahir <xahiru@gmail.com>
2025-03-14 22:03:19 +0800

    range_based loop

---
+++ slides/loops/range_based_loop.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> v { 1, 2, 3, 4, 5};
+    
+      // Using range based for loop to print vector
+      for (auto i: v) {
+          cout << i << " ";
+    }
+      
+    return 0;
+}
