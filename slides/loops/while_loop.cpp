3e3face168e8fc343518154309640004c8c35280
Ahmed Zahir <xahiru@gmail.com>
2025-03-14 22:03:57 +0800

    while loop example

---
+++ slides/loops/while_loop.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+      // Initialization
+      int i = 1;
+  
+      // while loop that starts with i = 1 and ends
+      // when i is greater than 5.
+    while (i <= 5) {
+        cout << i << " ";
+      
+          // Updation
+          i++;
+    }
+
+    return 0;
+}
