2c1b50d40fcb40eef0702242e7c87734e8646e81
Ahmed Zahir <xahiru@gmail.com>
2025-03-19 23:01:36 +0800

    bitwise or

---
+++ slides/operators/bitwise_or.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 3;  // Binary: 0011
+    int y = 6;  // Binary: 0110
+
+    cout << (x | y); // Perform bitwise OR
+    return 0;
+}
