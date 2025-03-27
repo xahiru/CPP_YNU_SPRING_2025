49640786b70b0c6787874e65f820e09b055b427a
shimanto1025 <kazishimant069@gmail.com>
2025-03-27 19:20:15 +0800

    added ans of qeustion 5 - 12

---
+++ HKS0018/session1/q7.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int a;
+    cout<<"Enter a number:"<<endl;
+    cin>>a;
+
+    if (a/2 ==0){
+        cout<<"Even number"<<endl;
+    }else{
+        cout<<"Odd number"<<endl;
+    }
+
+    return 0;
+}
