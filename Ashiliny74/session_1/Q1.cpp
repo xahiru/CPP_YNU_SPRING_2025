d720868eafa8e0cde0c4a8ee7407095844a2a75f
Ashiliny <langaashiliny@gmail.com>
2025-04-02 05:34:11 -0700

    add Q2

--- Ashiliny74/session_1/Q1.cpp
+++ Ashiliny74/session_1/Q1.cpp
@@ -1,14 +1,6 @@
 #include <iostream> // Include the iostream library for input and output
 
 int main() {
-    int number; // Declare a variable to store the user's input
-
-    // Prompt the user to enter a number
-    std::cout << "Enter any Number: ";
-    std::cin >> number; // Read the number from the user
-
-    // Print the entered number
-    std::cout << "Entered Number: " << number << std::endl;
-
+    std::cout << "Hello World" << std::endl; // Print "Hello World" to the console
     return 0; // Return 0 to indicate that the program ended successfully
 }
