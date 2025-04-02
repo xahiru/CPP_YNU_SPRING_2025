9f59636a390ae252908dd2da67b8e258d746f8de
zaidbenslimane1 <zaid.benslimane.zb@gmail.com>
2025-04-02 11:16:59 +0000

    first session commit

---
+++ ZAID012/SESSION1/Q2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number; 
+    cout << "Enter any Number: ";
+    cin >> number;
+    cout << "Entered Number: " << number << endl;
+
+    return 0; 
+}
