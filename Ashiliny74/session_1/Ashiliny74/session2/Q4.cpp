024ac8df3663b0866f2a9e5787099fabb6870477
Ashiliny <langaashiliny@gmail.com>
2025-04-08 05:24:20 -0700

    add Q4

---
+++ Ashiliny74/session_1/Ashiliny74/session2/Q4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double radius;
+
+    // Input radius
+    cout << "Find the area and circumference of any circle :" << endl;
+    cout << "----------------------------------------------------" << endl;
+    cout << "Input the radius (1/2 of diameter) of a circle : ";
+    cin >> radius;
+
+    // Calculate area and circumference
+    double area = M_PI * radius *_
