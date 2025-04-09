1aafa07b2f4e5f3d6d9368a76acb234c08a095de
shimanto1025 <kazishimant069@gmail.com>
2025-04-09 19:19:42 +0800

    session 2 completed

--- HKS0018/session2/q1.cpp
+++ HKS0018/session2/q1.cpp
@@ -3,16 +3,16 @@
 
 using namespace std;
 
+#define PI 3.1415
+
 
 int main(){
     double r;
-    double PI;
     double s;
 
     cout<<"Enter radius: "<<endl;
     cin>>r;
     r = r*r*r;
-    PI = 3.14159;
     s = (4.0/3.0)*PI*r;
 
     cout<<"The volume of sphere is " <<s<<endl;
