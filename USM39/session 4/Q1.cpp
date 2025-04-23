99fd5c75772af5aa940bc5c9e85fee2f24f99b8f
OnlyMrUsman <usmanch7829@gmail.com>
2025-04-23 16:29:27 +0500

    Update Q1.cpp

--- USM39/session 4/Q1.cpp
+++ USM39/session 4/Q1.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int main() {
     int a = 4;
-    cout << a << endl;  // Output: 4
+    cout << a << endl; 
     a = 61;
-    cout << a << endl;  // Output: 61
+    cout << a << endl;  
     return 0;
 }
