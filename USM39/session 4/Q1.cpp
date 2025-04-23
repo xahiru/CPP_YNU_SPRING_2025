707756e43f8c02547d166c0464857b89b90cb98a
OnlyMrUsman <usmanch7829@gmail.com>
2025-04-23 16:28:13 +0500

    Create Q1.cpp

---
+++ USM39/session 4/Q1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 4;
+    cout << a << endl;  // Output: 4
+    a = 61;
+    cout << a << endl;  // Output: 61
+    return 0;
+}
