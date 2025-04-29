0558ded38b4f4312d43aeabb0d61e73d165604fd
Fahadbcd17 <fahadbcd17@gmail.com>
2025-04-29 10:48:18 +0800

    lab-4 half

---
+++ fahad_02/lab_4/Q5_dynamic_memory.cpp
@@ -0,0 +1,12 @@
+#include<iostream>>
+using namespace std;
+
+int main(){
+    int a =5;
+    char b = 'a';
+    string c = "hello";
+
+    int *ptr1 = &a;
+    char *ptr2 = &b;
+    string *ptr3 = &c;
+}
