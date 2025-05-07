2154136a04ec1c01d73a43d99f2ddf4eb0a77e10
LesegoShozi <lesegoshozi935@outlook.com>
2025-05-07 19:27:03 +0800

    Add files via upload

---
+++ LSG_24/Session4/q18.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int binaryToDecimal(string binary) {
+    int decimal = 0;
+    int power = 0;
+    
+    for (int i = binary.length() - 1; i >= 0; i--) {
+        if (binary[i] == '1') {
+            decimal += pow(2, power);
+        }
+        power++;
+    }
+    
+    return decimal;
+}
+
+int main() {
+    string binary = "101011";
+    cout << "Output: " << binaryToDecimal(binary);
+    return 0;
+}
