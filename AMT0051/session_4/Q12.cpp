0a6221d1f90c3c428ff8ef5fef334d634bf1129f
alimdtuhin <alimdtuhin2015@gmail.com>
2025-05-12 14:19:56 +0800

    Q1 And q12

---
+++ AMT0051/session_4/Q12.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Size of int: 10 bytes" << endl;
+    cout << "Size of float: 20 bytes" << endl;
+    cout << "Size of double: 30 bytes" << endl;
+    cout << "Size of char: 40 bytes" << endl;
+
+    return 0;
+}
