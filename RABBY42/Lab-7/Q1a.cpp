a51f7a52e0c7df37d38b4b57cdc1b508ae5262bb
Rabby0501 <golamrabby213@gmail.com>
2025-05-21 20:08:43 +0800

    add Q1a & Q1b

---
+++ RABBY42/Lab-7/Q1a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+bool isPalindrome(string s) {
+    string rev = s;
+    reverse(rev.begin(), rev.end());
+    return s == rev;
+}
+
+int main() {
+    string s;
+    cin >> s;
+    cout << (isPalindrome(s) ? "Palindrome" : "Not a palindrome");
+    return 0;
+}
