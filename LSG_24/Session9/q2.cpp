6689f92ff7cd8b3a2766e653188dce053eebfe15
LesegoShozi <lesegoshozi935@outlook.com>
2025-05-24 14:00:58 +0800

    Add files via upload

---
+++ LSG_24/Session9/q2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class Rectangle {
+private:
+    double length;
+    double width;
+public:
+    void setDimensions(double l, double w) {
+        length = l;
+        width = w;
+    }
+    double getArea() {
+        return length * width;
+    }
+    double getPerimeter() {
+        return 2 * (length + width);
+    }
+};
+
+int main() {
+    Rectangle r;
+    r.setDimensions(4.0, 6.0);
+    cout << "Area: " << r.getArea() << endl;
+    cout << "Perimeter: " << r.getPerimeter() << endl;
+    return 0;
+}
